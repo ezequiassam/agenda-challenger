1c9bdc57b26dbf6dedd8de577af806b340c8b3ea
ezequ <ezequ@192.168.25.7>
2018-01-25 12:16:24 -0300

    up

--- src/main/java/com/br/agendachallenger/controller/AgendaController.java
+++ src/main/java/com/br/agendachallenger/controller/AgendaController.java
@@ -3,9 +3,13 @@ package com.br.agendachallenger.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,10 +25,19 @@ public class AgendaController {
 	@Autowired
 	private AgendaRepository provider;
 
-	@GetMapping(produces="application/json")
+	@GetMapping(produces = "application/json")
 	public @ResponseBody Iterable<Agenda> findAll() {
 		Iterable<Agenda> listaAgendas = provider.findAll();
 		return listaAgendas;
+	}
+
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Agenda> findById(@PathVariable("id") long id) {
+		Agenda agenda = provider.findOne(id);
+		if (agenda == null) {
+			return new ResponseEntity<Agenda>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Agenda>(agenda, HttpStatus.OK);
 	}
 
 	@PostMapping()
@@ -32,12 +45,26 @@ public class AgendaController {
 		return provider.save(agenda);
 
 	}
-	
-	@DeleteMapping()
-	public Agenda remove(@RequestBody Agenda agenda) {
+
+	@PutMapping(value = "/{id}", produces="application/json")
+	public Agenda update(@PathVariable("id") long id, @RequestBody @Valid Agenda agenda) {
+		Agenda a  = provider.findOne(id);
+		if (a == null) {
+			return null;
+		}
+		
+		a.setNome(agenda.getNome());
+		a.setTelefone(agenda.getTelefone());
+		a.setData(agenda.getData());
+		a.setLocal(agenda.getLocal());
+		return provider.save(a);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public Agenda remove(@PathVariable("id") long id) {
+		Agenda agenda = provider.findOne(id);
 		provider.delete(agenda);
 		return agenda;
 	}
-	
-	
+
 }
--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,8 +1,61 @@
-angular.module('demo', [])
-.controller('Hello', function($scope, $http) {
+angular.module('agenda', []).controller('portalController',
+		function($scope, $http) {
 
-	$http.get('http://localhost:8080/agenda').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
-});
+			var _apiHost = 'localhost:8080/agenda';
+//			$http.get('http://' + _apiHost).then(function(response) {
+//				$scope.allAgenda = response.data;
+//			});
+			
+			$scope.findAll = function() {
+				$http.get('http://' + _apiHost).then(function(response) {
+					$scope.allAgenda = response.data;
+				});
+			}
+			
+			$scope.findById =  function findById(id) {
+				 $http.get(_apiHost + '/'+ id).then(function(response) {
+				 return response.data;
+				 });
+			}
+			
+		});
+
+// angular.module('agenda').factory('portalService', ['$http', function ($http)
+// {
+//	
+// var _apiHost = 'http://localhost:8080/agenda';
+//	
+// return {
+//		
+// findAll: function() {
+// $http.get(_apiHost).then(function(response) {
+// return response.data;
+// });
+// }
+//				
+// findById: function(id) {
+// $http.delete(_apiHost + '/'+ id).then(function(response) {
+// return response.data;
+// });
+// }
+
+// createAgenda: function(params) {
+// $http.post(_apiHost, params).then(function(response) {
+// return response.data;
+// });
+// }
+//		
+// updateAgenda: function(id, params) {
+// $http.put(_apiHost + '/' + id, params).then(function(response) {
+// return response.data;
+// });
+// }
+//		
+// deleteAgenda: function(id) {
+// $http.delete(_apiHost + '/'+ id).then(function(response) {
+// return response.data;
+// });
+// }
+// }
+
+// }]);
